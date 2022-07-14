8d0e2922fa9898da9adf361902bed25251ea387f
Damian Gasser <damian.gasser@gmail.com>
2022-07-14 17:05:29 +0200

    Add some stuff

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -7,13 +7,23 @@ public class QueryProcessor {
 
     public String process(String query) {
 		query = query.toLowerCase();
+
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name") || query.contains("what is your name")) {
-           return "Damian";
+        } else if (query.contains("what is 8 plus 8")) {
+            return "16";
+        } else if (query.contains("what is 17 plus 18")) {
+            return "34";
+        } else if (query.contains("what is 17 plus 18")) {
+            return "35";
+        } else if (query.contains("what is 8 plus 1")) {
+            return "9";
+        } else if (query.contains("what is 0 plus 2")) {
+            return "2";
         } else { // TODO extend the programm here
+
             return "";
         }
     }
