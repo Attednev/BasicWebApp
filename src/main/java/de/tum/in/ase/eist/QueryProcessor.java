d21300faf1cc646a3ba0b553fe5111880e1246d6
Damian Gasser <damian.gasser@gmail.com>
2022-07-14 17:11:55 +0200

    Add some stuff

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,16 +12,28 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("what is 8 plus 8")) {
-            return "16";
-        } else if (query.contains("what is 17 plus 18")) {
-            return "34";
-        } else if (query.contains("what is 17 plus 18")) {
-            return "35";
-        } else if (query.contains("what is 8 plus 1")) {
-            return "9";
-        } else if (query.contains("what is 0 plus 2")) {
-            return "2";
+        } else if (query.contains("what is ")) {
+            if (query.contains(" plus ")) {
+                String p = query.split("what is ")[0];
+                String n1 = p.split(" plus ")[0];
+                String n2 = p.split(" plus ")[1];
+                return (Integer.parseInt(n1) + Integer.parseInt(n2)) + "";
+            }
+        } else if (query.contains("which of the following numbers is the largest: ")) {
+            String str = query.split("which of the following numbers is the largest: ");
+            String[] numbers = str.split(", ");
+            int max = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                if (i == 0) {
+                    max = Integer.parseInt(numbers[i]);
+                } else {
+                    int num = Integer.parseInt(numbers[i]);
+                    if (num > max) {
+                        max = num;
+                    }
+                }
+            }
+            return "" + max;
         } else { // TODO extend the programm here
 
             return "";
